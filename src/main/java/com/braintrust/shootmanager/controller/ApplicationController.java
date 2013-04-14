a78d818db21d2892fe20eea8c6b29e31e6f9939f
Andrew Larkin <andrewlarkin2@gmail.com>
2013-04-14 15:12:39 -0400

    Updating shootList page and adding route to get shoots

--- src/main/java/com/braintrust/shootmanager/controller/ApplicationController.java
+++ src/main/java/com/braintrust/shootmanager/controller/ApplicationController.java
@@ -1,18 +1,47 @@
 package com.braintrust.shootmanager.controller;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.braintrust.shootmanager.manager.PhotographerManager;
+import com.braintrust.shootmanager.manager.ShootManager;
 import com.braintrust.shootmanager.model.Photographer;
+import com.braintrust.shootmanager.model.Shoot;
+import com.google.gson.Gson;
 
 @Controller
 public class ApplicationController {
 	
+	private static Gson gson = new Gson();
+	
 	@RequestMapping("/")
-	public ModelAndView photographer() {
-		Photographer photographer = PhotographerManager.getPhotographer();
-		return new ModelAndView("shootList", "photographer", photographer);
+	public ModelAndView photographer() throws Exception {
+		try{
+			List<Photographer> photographers = PhotographerManager.getPhotographers();
+			return new ModelAndView("shootList", "photographers", photographers);
+		} catch (Exception e){
+			System.out.println("couldn't load database driver");           
+		}
+		return null;
+	}
+	
+	@RequestMapping(value = "/getShoots")
+	public @ResponseBody String getShoots(@RequestParam int pId) throws SQLException{
+		try {
+			List<Shoot> shoots = ShootManager.getShoots(pId); 
+			
+			System.out.println(shoots.size());
+			
+			return gson.toJson(shoots);
+		} catch (Exception e){
+			System.out.println("couldn't load database driver");           
+		}
+		return null;
 	}
 }
